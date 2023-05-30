a89319abe6e4d6586a8d0506a56e4f5da866d2bd
Jonas Wunderlich <git@03j.de>
2023-05-30 14:30:48 +0200

    rename last_line to previous_line_number

--- src/foundry_dev_tools/utils/misc.py
+++ src/foundry_dev_tools/utils/misc.py
@@ -13,37 +13,37 @@ class TailHelper:
             or a :py:class:logging.Logger
         start_message (str | None): the start_message to print before the first line
             or if None it will print a horizontal line
-        last_line (int | None): the last line that was printed, if not none the start_message
-            will never be printed and the first line will be the line after :py:attr:last_line
+        previous_line_number (int | None): the previous line number that was printed, if not none the start_message
+            will never be printed and the first line will be the line after :py:attr:previous_line_number
     """
 
     print_handler: "Callable[[str], Any]"
     start_message: "str | None" = None
-    last_line: "int | None" = None
+    previous_line_number: "int | None" = None
 
     def tail(self, log_lines: "Sequence[str] | None"):
         """Handles the logs.
 
-        If :py:attr:TailHelper.last_line is None it will print the :py:attr:TailHelper.start_message
+        If :py:attr:TailHelper.previous_line_number is None it will print the :py:attr:TailHelper.start_message
         and print everything contained int :py:attr:logs
-        But if the :py:attr:TailHelper.last_line is an integer it will print the
+        But if the :py:attr:TailHelper.previous_line_number is an integer it will print the
         logs from that given line number, this will only print new lines.
 
         Args:
             log_lines (Sequence[str] | None): a list of lines, if None execution will be skipped
         """
         if log_lines is not None:
-            if self.last_line is None:
+            if self.previous_line_number is None:
                 if self.start_message:
                     self.print_handler(self.start_message)
                 else:
                     print_horizontal_line(print_handler=self.print_handler)
                 self.print_handler("\n".join(log_lines))
             else:
-                new_lines = log_lines[self.last_line :]
+                new_lines = log_lines[self.previous_line_number :]
                 if new_lines:
                     self.print_handler("\n".join(new_lines))
-            self.last_line = len(log_lines)
+            self.previous_line_number = len(log_lines)
 
 
 def print_horizontal_line(c: str = "-", print_handler: "Callable[[str],Any]" = print):
