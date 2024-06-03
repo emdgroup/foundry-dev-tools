732dc84516b9b683e0da86d404d8e3fd0e704eb4
Lukas Fleckenstein <lukas.fleckenstein@merckgroup.com>
2024-06-03 16:47:25 +0200

    Move get_branch from dataset api to repo utils and add test

--- tests/test_utils_repo.py
+++ tests/test_utils_repo.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import uuid
 from pathlib import Path
@@ -5,7 +6,8 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from foundry_dev_tools.utils.repo import get_repo, git_toplevel_dir
+from foundry_dev_tools.utils.repo import get_branch, get_repo, git_toplevel_dir
+from tests.utils import add_git_submodule
 
 if TYPE_CHECKING:
     import py.path
@@ -24,44 +26,65 @@ def test_git_toplevel_dir(use_git: bool, tmpdir: "py.path.LocalPath", git_env: d
     assert git_dir_cwd == toplevel
 
     # Test if git submodules can be recognized as toplevel_dir
-    # First create an empty local repository and initialize it
-    subprocess.check_call(["git", "init", "dummy_repo"], cwd=toplevel, env=git_env)
-    subprocess.check_call(
-        [
-            "git",
-            "commit",
-            "--allow-empty",
-            "-m",
-            "Initialize",
-        ],
-        cwd=toplevel.joinpath("dummy_repo"),
-        env=git_env,
-    )
-
-    # Add local repository as submodule to toplevel repo
-    subprocess.check_call(
-        [
-            "git",
-            "-c",
-            "protocol.file.allow=always",
-            "submodule",
-            "add",
-            "./dummy_repo",
-            "dummy_submodule",
-        ],
-        cwd=toplevel,
-    )
-    toplevel_submodule = toplevel.joinpath("dummy_submodule")
+    submodule_name = "dummy_submodule"
+    add_git_submodule(git_dir=toplevel, submodule_name=submodule_name, git_env=git_env)
+    toplevel_submodule = toplevel.joinpath(submodule_name)
     git_dir_submodule = git_toplevel_dir(
-        Path(tmpdir.mkdir("dummy_submodule", "submodule_subdirectory")), use_git=use_git
+        Path(tmpdir.mkdir(submodule_name, "submodule_subdirectory")), use_git=use_git
     )
     assert git_dir_submodule == toplevel_submodule
+
+
+def test_get_branch(tmpdir: "py.path.LocalPath", git_env: dict):
+    toplevel = Path(tmpdir)
+
+    # Test without git submodule
+    parent_repo_name = "parent_repo"
+    target_branch_name_parent = "parent_repo_main"
+    subprocess.check_call(
+        [
+            "git",
+            "init",
+            parent_repo_name,
+            f"--initial-branch={target_branch_name_parent}",
+        ],
+        cwd=toplevel,
+        env=git_env,
+    )
+    branch_name = get_branch(toplevel.joinpath(parent_repo_name))
+    assert branch_name == target_branch_name_parent
+
+    # Test for git submodule
+    # Add an empty submodule
+    submodule_name = "dummy_submodule"
+    add_git_submodule(
+        git_dir=toplevel.joinpath(parent_repo_name),
+        submodule_name=submodule_name,
+        git_env=git_env,
+    )
+    # Checkout and test new branch from submodule
+    target_branch_name_sub = "submodule_repo_dev"
+    git_dir_submodule = toplevel.joinpath(parent_repo_name, submodule_name)
+    subprocess.check_call(
+        [
+            "git",
+            "checkout",
+            "-b",
+            target_branch_name_sub,
+        ],
+        cwd=git_dir_submodule,
+        env=git_env,
+    )
+    assert os.path.isfile(git_dir_submodule.joinpath(".git"))
+
+    branch_name = get_branch(git_dir_submodule)
+    assert branch_name == target_branch_name_sub
 
 
 def test_get_repo(tmpdir: "py.path.LocalPath", git_env: dict):
     repo_rid = f"ri.stemma.main.repository{uuid.uuid4()}"
     git_dir = Path(tmpdir)
-    subprocess.check_call(["git", "init"], cwd=git_dir)
+    subprocess.check_call(["git", "init"], cwd=git_dir, env=git_env)
     with git_dir.joinpath("gradle.properties").open("w+") as gradle_prop:
         gradle_prop.write(f"transformsRepoRid = {repo_rid}")
 
