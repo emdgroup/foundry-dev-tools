1c791bde35bc76ea1731a7917de58ffcb184e412
Silas Meister <silas.meister@merckgroup.com>
2024-08-12 08:07:19 +0200

    test(compass): write unit and integration tests for newly implemented endpoints

--- tests/integration/clients/test_multipass.py
+++ tests/integration/clients/test_multipass.py
@@ -3,13 +3,62 @@ Rotate Secret rotate_third_party_application_secret.
 
 """
 
+import functools
+from datetime import datetime, timedelta, timezone
+
 import pytest
 import requests
+from integration.utils import backoff
 
-from foundry_dev_tools.clients.multipass import DEFAULT_TOKEN_LIFETIME_IN_SECONDS
+from foundry_dev_tools.clients.multipass import (
+    DEFAULT_MAX_DURATION_IN_SECONDS,
+    DEFAULT_TOKEN_LIFETIME_IN_SECONDS,
+)
 from foundry_dev_tools.errors.meta import FoundryAPIError
 from foundry_dev_tools.foundry_api_client import FoundryRestClient
+from foundry_dev_tools.utils import api_types
 from tests.integration.conftest import TEST_SINGLETON
+
+TEST_GROUP_ID = "7bf29909-cb64-4353-a192-e5970e443909"
+
+DEV_WORKSPACE_OWNER_GROUP_ID = "e9a4a5e7-fbff-4856-ac6c-c657e97a73c2"
+DEV_WORKSPACE_VIEWER_GROUP_ID = "2ba614c8-65bb-4d1f-afa1-323610b755ec"
+
+
+def handle_forbidden_error(skip_message):
+    """Decorator function to handle 403 forbidden error when user lacks permission to skip the test.
+
+    Args:
+        skip_message: The message to pass on to :py:meth:`pytest.skip` when skipping a function
+    """
+
+    def decorator(test_function):
+        @functools.wraps(test_function)
+        def wrapper(*args, **kwargs):
+            try:
+                test_function(*args, **kwargs)
+            except FoundryAPIError as err:
+                if err.response.status_code == requests.codes.forbidden:
+                    pytest.skip(skip_message)
+                else:
+                    raise
+
+        return wrapper
+
+    return decorator
+
+
+def _get_group_name(group_id: api_types.GroupId) -> str:
+    """Helper function to retrieve the name of the specified group identifier.
+
+    Args:
+        group_id: The identifier of the group for which to retrieve the name
+
+    Returns:
+        str:
+            the name of the group
+    """
+    return TEST_SINGLETON.ctx.multipass.api_get_group(group_id).json()["name"]
 
 
 def test_crud(mocker):
@@ -165,3 +214,248 @@ def _test_token_endpoints_inner():
 
     assert response.status_code == 200
     assert is_revoked
+
+
+@handle_forbidden_error(
+    skip_message=(
+        "To test integration for multipass group administration endpoints, you need to be member of group "
+        f"`{_get_group_name(DEV_WORKSPACE_OWNER_GROUP_ID)}` ({DEV_WORKSPACE_OWNER_GROUP_ID})!"
+    )
+)
+def test_group_administrations():
+    user_id = TEST_SINGLETON.ctx.multipass.get_user_info()["id"]
+
+    sleep_time = 0.2
+
+    # Add user as `manager manager` to test group
+    resp = TEST_SINGLETON.ctx.multipass.add_group_manager_managers(TEST_GROUP_ID, {user_id})
+
+    assert resp.status_code == 204
+
+    successful, resp, _ = backoff(
+        lambda: TEST_SINGLETON.ctx.multipass.api_get_group_manager_managers(TEST_GROUP_ID),
+        lambda r: any(manager_manager["id"] == user_id for manager_manager in r.json()),
+        sleep_time,
+    )
+
+    assert successful
+    assert resp.status_code == 200
+
+    assert any(manager_manager["id"] == user_id for manager_manager in resp.json())
+
+    # Withdraw the role of `manager manager` from user for the test group
+    resp = TEST_SINGLETON.ctx.multipass.remove_group_manager_managers(TEST_GROUP_ID, {user_id})
+
+    assert resp.status_code == 204
+
+    successful, resp, _ = backoff(
+        lambda: TEST_SINGLETON.ctx.multipass.api_get_group_manager_managers(TEST_GROUP_ID),
+        lambda r: not any(manager_manager["id"] == user_id for manager_manager in r.json()),
+        sleep_time,
+    )
+
+    assert successful
+    assert resp.status_code == 200
+
+    assert not any(manager_manager["id"] == user_id for manager_manager in resp.json())
+
+    # Add user as `member manager` to test group
+    resp = TEST_SINGLETON.ctx.multipass.add_group_member_managers(TEST_GROUP_ID, {user_id})
+
+    assert resp.status_code == 204
+
+    successful, resp, _ = backoff(
+        lambda: TEST_SINGLETON.ctx.multipass.api_get_group_member_managers(TEST_GROUP_ID),
+        lambda r: any(member_manager["id"] == user_id for member_manager in r.json()),
+        sleep_time,
+    )
+
+    assert successful
+    assert resp.status_code == 200
+
+    assert any(member_manager["id"] == user_id for member_manager in resp.json())
+
+    # Withdraw the role of `member manager` from user for the test group
+    resp = TEST_SINGLETON.ctx.multipass.remove_group_member_managers(TEST_GROUP_ID, {user_id})
+
+    assert resp.status_code == 204
+
+    successful, resp, _ = backoff(
+        lambda: TEST_SINGLETON.ctx.multipass.api_get_group_member_managers(TEST_GROUP_ID),
+        lambda r: not any(member_manager["id"] == user_id for member_manager in r.json()),
+        sleep_time,
+    )
+
+    assert successful
+    assert resp.status_code == 200
+
+    assert not any(member_manager["id"] == user_id for member_manager in resp.json())
+
+
+@handle_forbidden_error(
+    skip_message=(
+        "To test integration for multipass group member administration endpoints, you need to be member of group "
+        f"`{_get_group_name(DEV_WORKSPACE_OWNER_GROUP_ID)}` ({DEV_WORKSPACE_OWNER_GROUP_ID})!"
+    )
+)
+def test_group_member_administration():
+    user_id = TEST_SINGLETON.ctx.multipass.get_user_info()["id"]
+
+    # Add user as member to test group
+    resp = TEST_SINGLETON.ctx.multipass.api_add_group_members({TEST_GROUP_ID}, {user_id})
+
+    assert resp.status_code == 204
+
+    # Additionally make the test group a member of the dev workspace viewer group
+    resp = TEST_SINGLETON.ctx.multipass.api_add_group_members({DEV_WORKSPACE_VIEWER_GROUP_ID}, {TEST_GROUP_ID})
+
+    assert resp.status_code == 204
+
+    # When retrieving immediate group members for dev workspace viewer group assert it does not contain the user
+    resp = TEST_SINGLETON.ctx.multipass.api_get_immediate_group_members(DEV_WORKSPACE_VIEWER_GROUP_ID)
+
+    assert resp.status_code == 200
+
+    group_members = resp.json()
+
+    assert any(member["id"] == TEST_GROUP_ID for member in group_members)
+    assert not any(member["id"] == user_id for member in group_members)
+
+    # Assert that the user has been added as member to test group and is indirect member of dev workspace viewer group
+    resp = TEST_SINGLETON.ctx.multipass.api_get_all_group_members({TEST_GROUP_ID, DEV_WORKSPACE_VIEWER_GROUP_ID})
+
+    assert resp.status_code == 200
+
+    group_id_members_mapping = resp.json()["membersByGroupId"]
+
+    assert TEST_GROUP_ID in group_id_members_mapping
+    assert any(member["principalId"] == user_id for member in group_id_members_mapping[TEST_GROUP_ID])
+
+    assert DEV_WORKSPACE_VIEWER_GROUP_ID in group_id_members_mapping
+    assert any(
+        member["principalId"] == TEST_GROUP_ID for member in group_id_members_mapping[DEV_WORKSPACE_VIEWER_GROUP_ID]
+    )
+    assert any(member["principalId"] == user_id for member in group_id_members_mapping[DEV_WORKSPACE_VIEWER_GROUP_ID])
+
+    # Assert that users of dev workspace viewer group contain at least all known user members of the dev workspace group
+    # but none of group members
+    resp = TEST_SINGLETON.ctx.multipass.api_get_all_group_users(DEV_WORKSPACE_VIEWER_GROUP_ID)
+
+    assert resp.status_code == 200
+
+    users = [user["id"] for user in resp.json()]
+    user_members = [
+        member["principalId"]
+        for member in group_id_members_mapping[DEV_WORKSPACE_VIEWER_GROUP_ID]
+        if member["principalType"] == "USER"
+    ]
+    group_members = [
+        member["principalId"]
+        for member in group_id_members_mapping[DEV_WORKSPACE_VIEWER_GROUP_ID]
+        if member["principalType"] == "GROUP"
+    ]
+
+    assert len(users) >= len(group_members)
+    assert all(user in users for user in user_members)
+    assert not any(group in users for group in group_members)
+
+    # Remove test group as member from dev workspace viewer group
+    resp = TEST_SINGLETON.ctx.multipass.api_remove_group_members(DEV_WORKSPACE_VIEWER_GROUP_ID, {TEST_GROUP_ID})
+
+    assert resp.status_code == 204
+
+    # Verify that test group has been removed from the dev workspace viewer group
+    resp = TEST_SINGLETON.ctx.multipass.api_get_immediate_group_members(DEV_WORKSPACE_VIEWER_GROUP_ID)
+
+    assert resp.status_code == 200
+    assert not any(member["id"] == TEST_GROUP_ID for member in resp.json())
+
+    # Assert that user is not indirect member of dev workspace viewer group anymore
+    resp = TEST_SINGLETON.ctx.multipass.api_get_all_group_members({DEV_WORKSPACE_VIEWER_GROUP_ID})
+
+    assert resp.status_code == 200
+
+    group_id_members_mapping = resp.json()["membersByGroupId"]
+
+    assert DEV_WORKSPACE_VIEWER_GROUP_ID in group_id_members_mapping
+    assert not any(
+        member["principalId"] == user_id for member in group_id_members_mapping[DEV_WORKSPACE_VIEWER_GROUP_ID]
+    )
+
+    # Remove user from test group again
+    resp = TEST_SINGLETON.ctx.multipass.api_remove_group_members(TEST_GROUP_ID, {user_id})
+
+    assert resp.status_code == 204
+
+    # Verify that user has been removed from test group
+    resp = TEST_SINGLETON.ctx.multipass.api_get_immediate_group_members(TEST_GROUP_ID)
+
+    assert resp.status_code == 200
+    assert not any(member["id"] == user_id for member in resp.json())
+
+
+@handle_forbidden_error(
+    skip_message=(
+        "To test integration for multipass group member expiration endpoints, you need to be member of group "
+        f"`{_get_group_name(DEV_WORKSPACE_OWNER_GROUP_ID)}` ({DEV_WORKSPACE_OWNER_GROUP_ID})!"
+    )
+)
+def test_group_member_expiration():
+    user_id = TEST_SINGLETON.ctx.multipass.get_user_info()["id"]
+
+    # Add user as member to test group
+    expiration_date = datetime.now(timezone.utc).replace(microsecond=0) + timedelta(days=7)
+    expirations = {TEST_GROUP_ID: {user_id: expiration_date}}
+
+    resp = TEST_SINGLETON.ctx.multipass.api_add_group_members({TEST_GROUP_ID}, {user_id}, expirations)
+
+    assert resp.status_code == 204
+
+    # Check that the expiration has been applied for the membership in test group
+    resp = TEST_SINGLETON.ctx.multipass.api_get_group_member_expirations({TEST_GROUP_ID})
+
+    assert resp.status_code == 200
+
+    expirations = resp.json()["expirationsByGroupId"]
+
+    assert TEST_GROUP_ID in expirations
+    assert "expirationsByPrincipalId" in expirations[TEST_GROUP_ID]
+
+    principal_expirations = expirations[TEST_GROUP_ID]["expirationsByPrincipalId"]
+
+    assert user_id in principal_expirations
+    assert datetime.fromisoformat(principal_expirations[user_id]["expiration"]) == expiration_date
+
+    # Remove user as member from test group
+    resp = TEST_SINGLETON.ctx.multipass.api_remove_group_members(TEST_GROUP_ID, {user_id})
+
+    assert resp.status_code == 204
+
+
+@handle_forbidden_error(
+    skip_message=(
+        "To test integration for multipass group member expiration settings endpoints, you need to be member of group "
+        f"`{_get_group_name(DEV_WORKSPACE_OWNER_GROUP_ID)}` ({DEV_WORKSPACE_OWNER_GROUP_ID})!"
+    )
+)
+def test_group_member_expiration_settings():
+    # Update expiration settings for test group
+    max_expiration = datetime.now(timezone.utc) + timedelta(seconds=DEFAULT_MAX_DURATION_IN_SECONDS)
+    max_duration_in_seconds = DEFAULT_MAX_DURATION_IN_SECONDS
+
+    resp = TEST_SINGLETON.ctx.multipass.api_update_group_member_expiration_settings(
+        TEST_GROUP_ID, max_expiration, max_duration_in_seconds
+    )
+
+    assert resp.status_code == 200
+
+    expiration_settings = resp.json()
+
+    assert expiration_settings["maxExpiration"] == max_expiration.strftime("%Y-%m-%dT%H:%M:%SZ")
+    assert expiration_settings["maxDurationInSeconds"] == max_duration_in_seconds
+
+    # Reset group member expiration settings to initial state
+    expiration_settings = TEST_SINGLETON.ctx.multipass.reset_group_member_expiration_settings(TEST_GROUP_ID)
+
+    assert expiration_settings["maxExpiration"] is None
+    assert expiration_settings["maxDurationInSeconds"] is None
--- tests/unit/clients/test_multipass.py
+++ tests/unit/clients/test_multipass.py
@@ -1,4 +1,5 @@
-from datetime import datetime, timezone
+import re
+from datetime import datetime, timedelta, timezone
 from random import choice
 from unittest.mock import patch
 
@@ -6,11 +7,155 @@ import pytest
 from freezegun import freeze_time
 
 from foundry_dev_tools.clients.multipass import (
+    DEFAULT_MAX_DURATION_IN_SECONDS,
     MAXIMUM_TOKEN_PAGE_SIZE,
+    MINIMUM_MAX_DURATION_IN_SECONDS,
     MINIMUM_TOKEN_PAGE_SIZE,
 )
 from foundry_dev_tools.utils.clients import build_api_url
 from tests.unit.mocks import TEST_HOST
+
+TEST_GROUP_ID = "abcdef01-2345-6789-abcd-ef0123456789"
+TEST_USER_ID = "a9b8c7d6-e5f4-3210-0f1e-2d3c4b5a6789"
+
+
+@freeze_time("0s")
+@patch("foundry_dev_tools.clients.api_client.APIClient.api_request")
+def test_api_add_group_members_expirations(api_request, test_context_mock):
+    test_context_mock.mock_adapter.register_uri(
+        "POST", build_api_url(TEST_HOST.url, "multipass", "administration/groups/bulk/members")
+    )
+
+    # Assert that expirations config for expiration in the past raises an exception
+    expirations = {TEST_GROUP_ID: {TEST_USER_ID: datetime.now(timezone.utc)}}
+
+    with pytest.raises(ValueError):  # noqa: PT011
+        test_context_mock.multipass.api_add_group_members({TEST_GROUP_ID}, {TEST_USER_ID}, expirations)
+
+    api_request.assert_not_called()
+
+    # Check that expirations without timezones raise a warning and are updated with utc timezone
+    max_expiration = datetime.now().replace(microsecond=0) + timedelta(seconds=DEFAULT_MAX_DURATION_IN_SECONDS)  # noqa: DTZ005
+    expirations = {TEST_GROUP_ID: {TEST_USER_ID: max_expiration}}
+
+    with pytest.warns():
+        test_context_mock.multipass.api_add_group_members({TEST_GROUP_ID}, {TEST_USER_ID}, expirations)
+
+    max_expiration = datetime.fromisoformat(
+        api_request.call_args.kwargs["json"]["expirations"][TEST_GROUP_ID][TEST_USER_ID]
+    )
+    max_expiration_expected = max_expiration.replace(tzinfo=timezone.utc)
+
+    assert max_expiration == max_expiration_expected
+
+
+@pytest.fixture()
+def _register_update_member_expiration_settings(test_context_mock):
+    test_context_mock.mock_adapter.register_uri(
+        "POST",
+        re.compile(
+            re.escape(build_api_url(TEST_HOST.url, "multipass", "groups/member-expiration-settings/groups/")) + "/.*"
+        ),
+    )
+
+
+@freeze_time("0s")
+@pytest.mark.usefixtures(_register_update_member_expiration_settings.__name__)
+@patch("foundry_dev_tools.clients.api_client.APIClient.api_request")
+def test_api_update_group_member_expiration_settings_max_expiration_in_past(api_request, test_context_mock):
+    now_utc = datetime.now(timezone.utc)
+
+    # Assert that expiration in the past is reset to the datetime of the beginning of the next day
+    max_expiration = now_utc - timedelta(seconds=DEFAULT_MAX_DURATION_IN_SECONDS)
+    expected_max_expiration = (now_utc + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+
+    with pytest.warns():
+        test_context_mock.multipass.api_update_group_member_expiration_settings(TEST_GROUP_ID, max_expiration)
+
+    request_body = api_request.call_args.kwargs["json"]
+
+    assert datetime.fromisoformat(request_body["maxExpiration"]) == expected_max_expiration
+
+    # Assert that expiration date in the moment of time is also reset to the datetime of the beginning of the next day
+    max_expiration = now_utc
+    expected_max_expiration = (now_utc + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+
+    with pytest.warns():
+        test_context_mock.multipass.api_update_group_member_expiration_settings(TEST_GROUP_ID, max_expiration)
+
+    request_body = api_request.call_args.kwargs["json"]
+
+    assert datetime.fromisoformat(request_body["maxExpiration"]) == expected_max_expiration
+
+    # Check that max_expiration without timezone throws warning for missing timezone
+    max_expiration = datetime.now() + timedelta(seconds=DEFAULT_MAX_DURATION_IN_SECONDS)  # noqa: DTZ005
+
+    with pytest.warns():
+        test_context_mock.multipass.api_update_group_member_expiration_settings(TEST_GROUP_ID, max_expiration)
+
+
+@pytest.mark.parametrize(
+    "max_expiration",
+    [
+        datetime.now().astimezone() + timedelta(seconds=DEFAULT_MAX_DURATION_IN_SECONDS),
+        datetime.now(timezone.utc) + timedelta(seconds=DEFAULT_MAX_DURATION_IN_SECONDS),
+    ],
+)
+@pytest.mark.usefixtures(_register_update_member_expiration_settings.__name__)
+@patch("foundry_dev_tools.clients.api_client.APIClient.api_request")
+def test_api_update_group_member_expiration_settings_different_timezones(
+    api_request, max_expiration, test_context_mock
+):
+    # Check whether different time zones are handled correctly
+    test_context_mock.mock_adapter.register_uri(
+        "POST",
+        re.compile(
+            re.escape(build_api_url(TEST_HOST.url, "multipass", "groups/member-expiration-settings/groups/")) + "/.*"
+        ),
+    )
+
+    # Assert that time zone of max_expiration gets converted into UTC time zone before being sent
+    expected_max_expiration = max_expiration.astimezone(timezone.utc).replace(microsecond=0)
+
+    test_context_mock.multipass.api_update_group_member_expiration_settings(TEST_GROUP_ID, max_expiration)
+
+    request_body = api_request.call_args.kwargs["json"]
+
+    assert datetime.fromisoformat(request_body["maxExpiration"]) == expected_max_expiration
+
+
+@pytest.mark.usefixtures(_register_update_member_expiration_settings.__name__)
+@patch("foundry_dev_tools.clients.api_client.APIClient.api_request")
+def test_api_update_group_member_expiration_settings_invalid_max_durations(api_request, test_context_mock):
+    # Choose invalid max duration that will be reset to default value
+    invalid_max_duration = MINIMUM_MAX_DURATION_IN_SECONDS - 1
+    with pytest.warns():
+        test_context_mock.multipass.api_update_group_member_expiration_settings(
+            TEST_GROUP_ID, max_duration_in_seconds=invalid_max_duration
+        )
+
+    request_body = api_request.call_args.kwargs["json"]
+
+    assert request_body["maxExpiration"] is None
+    assert request_body["maxDurationInSeconds"] == DEFAULT_MAX_DURATION_IN_SECONDS
+
+
+@pytest.mark.parametrize(
+    "max_duration_in_seconds", [MINIMUM_MAX_DURATION_IN_SECONDS, choice(range(DEFAULT_MAX_DURATION_IN_SECONDS))]
+)
+@pytest.mark.usefixtures(_register_update_member_expiration_settings.__name__)
+@patch("foundry_dev_tools.clients.api_client.APIClient.api_request")
+def test_api_update_group_member_expiration_settings_valid_max_durations(
+    api_request, max_duration_in_seconds, test_context_mock
+):
+    test_context_mock.multipass.api_update_group_member_expiration_settings(
+        TEST_GROUP_ID, max_duration_in_seconds=max_duration_in_seconds
+    )
+
+    request_body = api_request.call_args.kwargs["json"]
+
+    assert request_body["maxExpiration"] is None
+    assert request_body["maxDurationInSeconds"] == max_duration_in_seconds
 
 
 @patch("foundry_dev_tools.clients.api_client.APIClient.api_request")
