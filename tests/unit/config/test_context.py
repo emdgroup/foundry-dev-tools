cb60eda2e4d81864407d0be57083d05d930f06bf
Jonas Wunderlich <jonas.wunderlich@merckgroup.com>
2024-08-19 16:51:29 +0200

    tests: add app service foundry context test

---
+++ tests/unit/config/test_context.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import os
+from typing import TYPE_CHECKING
+from unittest import mock
+
+import pytest
+
+from foundry_dev_tools import FoundryContext
+from foundry_dev_tools.errors.config import FoundryConfigError
+from tests.unit.mocks import TEST_DOMAIN
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+def test_app_service(mock_config_location: dict[Path, None]):
+    with (
+        mock.patch.dict(os.environ, {"APP_SERVICE_TS": "1", "FDT_CREDENTIALS__DOMAIN": TEST_DOMAIN}, clear=True),
+        pytest.raises(
+            FoundryConfigError,
+            match="Could not get Foundry token from flask/dash/streamlit headers.",
+        ),
+    ):
+        _ = FoundryContext()
