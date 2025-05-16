d17d985ab868a9134c31756fee6b5bf51794c712
nicornk <nicornk@users.noreply.github.com>
2025-05-16 21:37:14 +0200

    refactor logic from ContextHttpClient into wrapping class to be api compatible to requests.Session

--- libs/foundry-dev-tools/src/foundry_dev_tools/clients/context_client.py
+++ libs/foundry-dev-tools/src/foundry_dev_tools/clients/context_client.py
@@ -4,17 +4,15 @@ from __future__ import annotations
 
 import logging
 import numbers
-import os
 import time
 import typing
-from typing import TYPE_CHECKING, Literal
+from typing import TYPE_CHECKING
 
 import requests
 
-from foundry_dev_tools.__about__ import __version__
-from foundry_dev_tools.errors.handling import ErrorHandlingConfig, raise_foundry_api_error
-
 if TYPE_CHECKING:
+    from os import PathLike
+
     from requests import Response
     from requests.cookies import RequestsCookieJar
     from requests.sessions import (  # type: ignore[attr-defined]
@@ -31,7 +29,6 @@ if TYPE_CHECKING:
         _Verify,
     )
 
-    from foundry_dev_tools.config.context import FoundryContext
 
 DEFAULT_TIMEOUT = (60, None)
 LOGGER = logging.getLogger(__name__)
@@ -73,25 +70,10 @@ def retry(times: int, exceptions: tuple[Exception]) -> typing.Callable:
 class ContextHTTPClient(requests.Session):
     """Requests Session with config and authentication applied."""
 
-    def __init__(self, context: FoundryContext, requests_session_overwrite: requests.Session | None = None) -> None:
-        self.context = context
-        if not requests_session_overwrite:
-            super().__init__()
-            self._requests_session = super()
-            self.auth = lambda r: self.context.token_provider.requests_auth_handler(r)
-            self.headers["User-Agent"] = requests.utils.default_user_agent(
-                f"foundry-dev-tools/{__version__}/python-requests"
-            )
-            if self.context.config.requests_ca_bundle:
-                self.verify = os.fspath(self.context.config.requests_ca_bundle)
-        else:
-            self._requests_session = requests_session_overwrite
-            self._requests_session.auth = lambda r: self.context.token_provider.requests_auth_handler(r)
-            self._requests_session.headers["User-Agent"] = requests.utils.default_user_agent(
-                f"foundry-dev-tools/{__version__}/python-requests"
-            )
-            if self.context.config.requests_ca_bundle:
-                self._requests_session.verify = os.fspath(self.context.config.requests_ca_bundle)
+    def __init__(self, debug: bool = False, requests_ca_bundle: PathLike[str] | None = None) -> None:
+        self.debug = debug
+        self.requests_ca_bundle = requests_ca_bundle
+        super().__init__()
 
         self._counter = 0
 
@@ -114,7 +96,6 @@ class ContextHTTPClient(requests.Session):
         verify: _Verify | None = None,
         cert: _Cert | None = None,
         json: Incomplete | None = None,
-        error_handling: ErrorHandlingConfig | Literal[False] | None = None,
     ) -> Response:
         """Make an authenticated HTTP request.
 
@@ -135,11 +116,10 @@ class ContextHTTPClient(requests.Session):
             verify: see :py:meth:`requests.Session.request`
             cert: see :py:meth:`requests.Session.request`
             json: see :py:meth:`requests.Session.request`
-            error_handling: error handling config; if set to False, errors won't be automatically handled
         """
-        if verify is None and (rcab := self.context.config.requests_ca_bundle) is not None:
+        if verify is None and (rcab := self.requests_ca_bundle) is not None:
             verify = rcab
-        if self.context.config.debug:
+        if self.debug:
             self._counter = count = self._counter + 1
             LOGGER.debug(f"(r{count}) Making {method!s} request to {url!s}")  # noqa: G004
 
@@ -150,7 +130,7 @@ class ContextHTTPClient(requests.Session):
             # set to the default connect timeout if it is none
             timeout = DEFAULT_TIMEOUT
 
-        response = self._requests_session.request(
+        response = super().request(
             method,
             url,
             params=params,
@@ -168,12 +148,11 @@ class ContextHTTPClient(requests.Session):
             cert=cert,
             json=json,
         )
-        if self.context.config.debug:
+        if self.debug:
             LOGGER.debug(
                 f"(r{count}) Got response status={response.status_code}, "  # noqa: G004
                 f"content_type={response.headers.get('content-type')}, "
                 f"content_length={response.headers.get('content-length')}, "
                 f"Server-Timing={response.headers.get('Server-Timing')}",
             )
-        raise_foundry_api_error(response, error_handling)
         return response
