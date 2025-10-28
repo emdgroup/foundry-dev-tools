3ab9ab1d87f64ed457d745345bc4d876ebcf80f5
Nico Mohr <nico.mohr@merckgroup.com>
2025-10-28 12:40:59 +0100

    add fakemodule wrapper around foundry_sdk

---
+++ libs/foundry-dev-tools/src/foundry_dev_tools/_optional/foundry_platform_sdk.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+try:
+    import foundry_sdk
+
+    foundry_sdk.__fake__ = False
+except ImportError:
+    from foundry_dev_tools._optional import FakeModule
+
+    foundry_sdk = FakeModule("foundry_sdk")
+
+__all__ = ["foundry_sdk"]
