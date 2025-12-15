78a1ac26d78dec23727648f2486230433e147d32
Wojciech Kuliński <77805222+misterbrunet@users.noreply.github.com>
2025-12-15 15:16:30 +0000

    Adding `DataHealthClient` & changelog update

---
+++ libs/foundry-dev-tools/src/foundry_dev_tools/clients/data_health.py
@@ -0,0 +1,60 @@
+"""Implementation of the data-health API."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List
+
+from foundry_dev_tools.clients.api_client import PublicAPIClient, APIClient
+
+if TYPE_CHECKING:
+    import requests
+
+    from foundry_dev_tools.utils import api_types
+
+
+class DataHealthClient(APIClient):
+    """DataHealthClient class that implements methods from the 'foundry-data-health' API."""
+
+    api_name = "data-health"
+
+    def api_get_checks_for_dataset(
+        self,
+        datasetRid: datasetRid,
+        branch: str = "master",
+        **kwargs,
+    ) -> requests.Response:
+        """Gets all the Check RIDs available for the given Dataset RID and branch name.
+
+        Args:
+            dataset_rid: The RID of the dataset
+            branch: The name of the branch
+            **kwargs: gets passed to :py:meth:`APIClient.api_request`
+        """
+        return self.api_request(
+            "GET",
+            api_path=f"checks/v2/{datasetRid}/{branch}",
+            **kwargs,
+        )
+    
+    def api_get_latest_checks_history(
+        self,
+        checkRids: List[CheckRid] | CheckRid,
+        limit: int | None = None,
+        **kwargs
+    ) -> requests.Response:
+        """Returns complete CheckRun metadata for the last `limit` number of check runs
+        for the given list of Check RIDs.
+
+        Args:
+            checkRids: a list of CheckRIDs or a single string value.
+            limit: number of last check runs to go for for each Check RID. Defaults to 5.
+            **kwargs: gets passed to :py:meth:`APIClient.api_request`
+        """
+        checkRids = checkRids if isinstance(checkRids, list) else [checkRids]
+        limit = limit if limit else 5
+        return self.api_request(
+            "POST",
+            api_path=f"checks/reports/v2/latest?limit={limit}",
+            json=checkRids,
+            **kwargs,
+        )
